8c6e0243f6ae52e9d8b13fdddb7ad7f5ed53463c
Void-Cat <Void-Cat@outlook.com>
2018-06-27 23:06:45 +0200

    Updated tease setup

---
+++ src/ts/setup/categories.ts
@@ -0,0 +1,135 @@
+class CategoryControl {
+    categories = {}
+    elements = {}
+    skipped : number[] = []
+    public length: number = 0
+
+    public add(name: string, element = true, count?: number, ratio?: number) : boolean {
+        if (this.skipped.length > 0)
+            var id = this.skipped.shift()
+        else
+            var id = Object.keys(this.categories).length
+        this.categories[id] = {
+            count: count || 0,
+            name: name,
+            ratio: ratio || 0,
+        }
+        this.length++
+        if (element)
+            return this.createElement(id, name, count, ratio)
+        return true
+    }
+
+    public clear(confirm = false) {
+        if (confirm) {
+            Object.keys(this.categories).forEach((id: any) => {
+                this.remove(id)
+            })
+        }
+    }
+
+    constructor(load = true) {
+        if (load) {
+            let cats = storage.get('tease.categories')
+            if (isObject(cats))
+                Object.keys(cats).forEach((id: any) => {
+                    let cat = cats[id]
+                    this.add(cat.name, true, cat.count, cat.ratio)
+                })
+        }
+    }
+
+    createElement(id: number, name: string, count?: number, ratio?: number) : boolean {
+        if (isNullOrUndefined(count)) count = 0
+        if (isNullOrUndefined(ratio)) ratio = 0
+        this.elements[id] = {
+            count: this._createElement(id, name, count, 'count'),
+            ratio: this._createElement(id, name, ratio, 'ratio')
+        }
+        $('#cardMode-manual-list').append(this.elements[id].count)
+        $('#cardMode-percentage-list').append(this.elements[id].ratio)
+        $(`input[name="category-${id}-name"]`).on('change', (event) => {
+            this.rename(id, $(event.currentTarget).val().toString())
+        })
+        $(`input[name="category-${id}-count"]`).on('change', (event) => {
+            this.categories[id].count = parseInt($(`input[name="category-${id}-count"]`).val().toString(), 10)
+        })
+        $(`input[name="category-${id}-ratio"]`).on('change', (event) => {
+            this.categories[id].ratio = parseInt($(`input[name="category-${id}-ratio"]`).val().toString(), 10)
+        })
+        $(`i[name="category-${id}-remove"]`).on('click', () => {
+            this.remove(id)
+            $(`div[name="category-${id}"]`).remove()
+        })
+        mdc.textField.MDCTextField.attachTo($(this.elements[id].count).children('.mdc-text-field')[0])
+        mdc.textField.MDCTextField.attachTo($(this.elements[id].count).children('.mdc-text-field')[1])
+        mdc.textField.MDCTextField.attachTo($(this.elements[id].ratio).children('.mdc-text-field')[0])
+        mdc.textField.MDCTextField.attachTo($(this.elements[id].ratio).children('.mdc-text-field')[1])
+        return true
+    }
+
+    _createElement(id: number, name: string, numerical: number, type: string) : JQuery<HTMLElement> {
+        let baseElement = `
+        <div name="category-${id}" class="mdc-list-item">
+            <div class="mdc-text-field">
+                <input name="category-${id}-name" value="${name}" type="text" class="mdc-text-field__input" />
+                <label class="mdc-floating-label" for="category-${id}-name">Category Name</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+            &nbsp;
+            <div class="mdc-text-field">
+                <input name="category-${id}-${type}" value="${numerical}" type="number" min="0" class="mdc-text-field__input" />
+                <label class="mdc-floating-label" for="category-${id}-${type}">Category ${type[0].toUpperCase()}${type.substring(1)}</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+            <i name="category-${id}-remove" class="material-icons" style="float: right; cursor: pointer;">delete</i>
+        </div>`
+        return $(baseElement).clone()
+    }
+
+    public remove(id: number) : boolean {
+        if (isNullOrUndefined(this.categories[id]))
+            return false;
+        delete this.categories[id]
+        $(this.elements[id].count).off().remove()
+        $(this.elements[id].ratio).off().remove()
+        delete this.elements[id]
+        this.skipped.push(id)
+        this.length--
+        return true;
+    }
+
+    public rename(id: number, newName: string) : boolean {
+        this.categories[id].name = newName
+        return true
+    }
+
+    public save() : void {
+        this.retrieveAll()
+        storage.set('tease.categories', this.categories)
+    }
+
+    public update(id?: number, count?: number, ratio?: number) : boolean {
+        if (isNullOrUndefined(id)) {
+            this.retrieveAll()
+            return true;
+        }
+        if (isNullOrUndefined(this.categories[id]) || (isNullOrUndefined(count) && isNullOrUndefined(ratio)))
+            return false;
+        if (!isNullOrUndefined(count))
+            this.categories[id].count = count
+        if (!isNullOrUndefined(ratio))
+            this.categories[id].ratio = ratio
+        return true;
+    }
+
+    public retrieveAll() : void {
+        Object.keys(this.elements).forEach((category : any) => {
+            this.update(
+                category,
+                parseInt($(`input[name="category-${category}-count"]`).val().toString(), 10),
+                parseInt($(`input[name="category-${category}-ratio"]`).val().toString(), 10)
+            )
+        })
+    }
+}
---
+++ src/ts/setup/setup.ts
@@ -0,0 +1,249 @@
+var { isNullOrUndefined, isObject, isBoolean, isString, isNumber, isArray } = require('util')
+
+declare const mdc, dialog, BrowserWindow, globalShortcut, swapper
+
+var categories
+
+/** Retrieves a folder path using a user dialog */
+function getFolder(title = 'Select Folder', label?: string, setconfig?: string, folder?: string) {
+    if (isNullOrUndefined(folder)) {
+        dialog.showOpenDialog({
+            title: title,
+            properties: ['openDirectory']
+        }, (path) => {
+            if (isArray(path))
+                path = path[0]
+
+            if (!isNullOrUndefined(setconfig)) storage.set(setconfig, path)
+            
+            if (!isNullOrUndefined(label)) {
+                if (path === '' || path === undefined) {
+                    $(label).fadeOut(100)
+                } else {
+                    $(label).text(path)
+                    $(label).fadeIn(100)
+                }
+            }
+            return path
+        })
+    } else {
+        if (!isNullOrUndefined(label)) {
+            $(label).text(folder)
+            $(label).fadeIn(100)
+        }
+    }
+}
+
+/** Retrieves a file path using a user dialog */
+function getFile(title = 'Select File', label?: string, setconfig?: string, extensions?: object[], file?: string) {
+    if (isNullOrUndefined(file)) {
+        dialog.showOpenDialog({
+            title: title,
+            filters: extensions,
+            properties: ['openFile']
+        }, (path) => {
+            if (isArray(path))
+                path = path[0]
+            
+            if (!isNullOrUndefined(setconfig)) storage.set(setconfig, path)
+
+            if (!isNullOrUndefined(label)) {
+                if (path === '' || path === undefined)
+                    $(label).fadeOut(100)
+                else {
+                    $(label).text(path)
+                    $(label).fadeIn(100)
+                }
+            }
+            return path
+        })
+    } else {
+        if (!isNullOrUndefined(label)) {
+            $(label).text(file)
+            $(label).fadeIn(100)
+        }
+    }
+}
+
+function loadSetup() {
+    let setup = storage.get('tease.setup')
+    if (isBoolean(setup['saveload']))
+        $('#saveSetup').prop('checked', setup['saveload'])
+    if (isString(setup['imagefolder']))
+        getFolder(undefined, '#selectImageFolderLabel', undefined, setup['imagefolder'])
+    if (isNumber(setup['imagecount']))
+        $('#pictureAmount').val(setup['imagecount'])
+    if (isNumber(setup['slidetime']))
+        $('#slideTime').val(setup['slidetime'])
+    if (isBoolean(setup['enableticker']))
+        $('#enableTicker').prop('checked', setup['enableticker'])
+    if (isString(setup['tickersound']))
+        $('#tickerSound option[value="' + setup['tickersound'] + '"]').prop('selected', true).siblings().prop('selected', false)
+    if (isBoolean(setup['announcecard']))
+        $('#announceCard').prop('checked', setup['announcecard'])
+    if (isBoolean(setup['announceimage']))
+        $('#announceImage').prop('checked', setup['announceimage'])
+    if (isString(setup['cardfolder']))
+        getFolder(undefined, '#selectCardFolderLabel', undefined, setup['cardfolder'])
+    if (isString(setup['cardmode'])) {
+        $('#cardModeSelection option[value="' + setup['cardmode'] + '"]').prop('selected', true).siblings().prop('selected', false)
+        $('#cardModeSelection').val(setup['cardmode'])
+        $('#cardModeSelection').change()
+    }
+    if (isString(setup['premade']))
+        getFile(undefined, '#selectPremadeDeckLabel', undefined, undefined, setup['premade'])
+    if (isString(setup['goal']))
+        $('#teaseGoalSelection').val(setup['goal'])
+    if (isNumber(setup['goalx']))
+        $('#teaseGoalX').val(setup['goalx'])
+    if (isBoolean(setup['infinite']))
+        $('#infiniteTease').prop('checked', setup['infinite'])
+    if (isBoolean(setup['blockexit']))
+        $('#teaseGoalQuitBlock').prop('checked')
+}
+
+function reportError(text: string, appendTo?: string) {
+    let id = (new Date).getTime().toString()
+    let object = `<div id="error-${id}" class="box mdc-theme--secondary-bg">
+        <h1 class="mdc-typography--body1">
+            ${text}&nbsp;
+            <span class="material-icons" style="float: right; cursor: pointer;" onclick="$('#error-${id}').remove()">close</span>
+        </h1>
+    </div>`
+    if (isNullOrUndefined(appendTo))
+        $(object).attr('style', 'position:fixed;top:34;').prependTo('#swapper')
+    else 
+        $(object).appendTo(appendTo)
+}
+
+function saveSetup() {
+    storage.set('tease.setup.saveload', ($('#saveSetup').prop('checked') || false))
+    if (isString($('#pictureAmount').val()) && $('#pictureAmount').val() !== "")
+        storage.set('tease.setup.imagecount', parseInt($('#pictureAmount').val().toString(), 10))
+    else
+        storage.set('tease.setup.imagecount', 0)
+    if (isString($('#slideTime').val()) && $('#slideTime').val() !== "")
+        storage.set('tease.setup.slidetime', parseInt($('#slideTime').val().toString(), 10))
+    else
+        storage.set('tease.setup.slidetime', 10)
+    storage.set('tease.setup.enableticker', ($('#enableTicker').prop('checked') || true))
+    storage.set('tease.setup.tickersound', $('#tickersound').val())
+    storage.set('tease.setup.announcecard', ($('#announceCard').prop('checked') || false))
+    storage.set('tease.setup.announceimage', ($('#announceImage').prop('checked') || false))
+    storage.set('tease.setup.cardmode', $('#cardModeSelection').val())
+    storage.set('tease.setup.goal', $('#teaseGoalSelection').val())
+    storage.set('tease.setup.goalx', parseInt($('#teaseGoalX').val().toString(), 10))
+    storage.set('tease.setup.infinite', $('#infiniteTease').prop('checked'))
+    storage.set('tease.setup.blockexit', $('#teaseGoalQuitBlock').prop('checked'))
+}
+
+// Setup page components
+$(document).ready(() => {
+    categories = new CategoryControl(storage.get('tease.setup.saveload') || undefined)
+
+    // Setup select menus
+    document.querySelectorAll('.mdc-select').forEach(el => {
+        mdc.select.MDCSelect.attachTo(el)
+        $(el).find('option').css('color', 'black')
+    })
+
+    // Setup text field
+    document.querySelectorAll('.mdc-text-field').forEach(el => {
+        mdc.textField.MDCTextField.attachTo(el)
+    })
+    
+    // Setup button ripples
+    document.querySelectorAll('.mdc-button, .mdc-fab').forEach(el => {
+        mdc.ripple.MDCRipple.attachTo(el)
+    })
+
+    // Setup folder buttons
+    $('#selectImageFolder').click(() => getFolder('Select Image Folder', '#selectImageFolderLabel', 'tease.setup.imagefolder'))
+    $('#selectCardFolder').click(() => getFolder('Select Cards Folder', '#selectCardFolderLabel', 'tease.setup.cardfolder'))
+    $('#selectPremadeDeck').click(() => getFile('Select Deck File', '#selectPremadeDeckLabel', 'tease.setup.premade', [{name: 'Deck', extensions: ['deck']}]))
+
+    // Setup saveload checkbox
+    $('#saveSetup').click(() => saveSetup())
+
+    // Setup card-mode switching
+    $('#cardModeSelection').change(() => {
+        $('#cardMode').children(':visible').slideUp(200)
+        $('#cardMode-' + $('#cardModeSelection').val()).slideDown(200)
+        if ($('#cardModeSelection').val() == 'premade')
+            $('#teaseGoalSetup').slideUp(200)
+        else
+            $('#teaseGoalSetup').slideDown(200)
+        if ($('#cardModeSelection').val() == 'none' || $('#cardModeSelection').val() == 'premade')
+            $('#addCategory, #resetCategories').prop('disabled', true).addClass('mdc-button--disabled')
+        else
+            $('#addCategory, #resetCategories').prop('disabled', false).removeClass('.mdc-button--disabled')
+    })
+
+    // Setup teaseGoal switching
+    $('#teaseGoalSelection').click(() => {
+        switch ($('#teaseGoalSelection').val()) {
+            case 'end':
+                $('#teaseGoalXContainer, #infiniteTeaseContainer').slideUp(200)
+                break
+            case 'cum':
+            case 'minutes':
+                $('#teaseGoalXContainer, #infiniteTeaseContainer').slideDown(200)
+                break
+            case 'release':
+                $('#teaseGoalXContainer').slideUp(200)
+                $('#infiniteTeaseContainer').slideDown(200)
+        }
+    })
+
+    // Load settings if allowed
+    if (storage.get('tease.setup.saveload'))
+        loadSetup()
+
+    // Setup add category button
+    $('#addCategory').click(() => {
+        if ($('#addCategory').prop('disabled') != true)
+            categories.add('New Category')
+    })
+
+    // Setup default categories button
+    $('#resetCategories').click(() => {
+        if ($('#resetCategory').prop('disabled') != true) {
+            categories.clear(true)
+            let defaults = ['Stroke It', 'Bondage', 'Rough Mistress', 'Delusional', 'Nice Mistress', 'Dilemma',
+                'Chance to Cum', 'Getting into Character', 'Special', 'Humiliation', 'Chastity Belt', 'Mind Control',
+                'Key', 'Work It', 'Edge', 'Time Lapse']
+            defaults.forEach((cat) => {
+                categories.add(cat)
+            })
+        }
+    })
+
+    // Setup tease start button
+    $('#startTease').click(() => {
+        if ($('#pictureAmount').val() == "0" || $('#pictureAmount').val() == "")
+            reportError('You haven\'t set an amount of pictures.', '#pictureSetup')
+        else if ($('#cardModeSelection').val() == 'none')
+            reportError('Select a card mode.', '#cardModeSelectionContainer')
+        else if ($('#cardModeSelection').val() != 'premade' && categories.length <= 0)
+            reportError('You have no card categories set up.', '#cardModeSelectionContainer')
+        else {
+            saveSetup()
+            categories.save()
+            var window = new BrowserWindow({
+                backgroundColor: '#000000',
+                frame: false,
+                height: 600,
+                show: false,
+                width: 800
+            })
+            window.loadURL(`file://${__dirname}/html/tease.html`)
+            window.setFullScreen(true)
+            globalShortcut.register('CommandOrControl+Shift+Y', () => window.webContents.toggleDevTools())
+            window.once('ready-to-show', () => window.show())
+            window.on('close', () => {
+                globalShortcut.unregister('CommandOrControl+Shift+Y')
+                swapper.swap('teaseend')
+            })
+        }
+    })
+})
